eddca814a76eb35e9540ae8221b2c89096fc8ff7
John Doe <johndoe@example.com>
2025-01-06 12:17:29 +0100

    Deployment changes

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,8 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql",
+  // uri: "http://localhost:8000/graphql",
+  uri: "https://two501-mern-graphql.onrender.com/graphql",
   cache,
 });
 
